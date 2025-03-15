06ad27b68a0188a6093710bab3850d2d42a8a64c
qirong <609413629@qq.com>
2025-03-15 16:54:45 +0800

    路径别名

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,22 @@
-import "./style.css";
-import './common/index'
-import './groups/solar'
-import './groups/background'
+// import "./style.css";
+// import './common/index'
+// import './groups/solar'
+// import './groups/background'
 
+import videoPath from '@assets/audio/bg.mp3'
 // 创建音频元素
 const audio = document.createElement('audio')
-audio.src = '../public/audio/bg.mp3'
+console.log(videoPath)
+// audio.src = videoPath;
+audio.src = videoPath;
+
 audio.loop = true
 audio.autoplay = true
 // 添加错误处理
 audio.onerror = (e) => {
     console.error('音频加载失败:', e)
 }
-
 document.body.appendChild(audio)
+document.addEventListener('click', () => {
+    audio.play()
+})
