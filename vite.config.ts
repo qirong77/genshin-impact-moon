1b56708ab87adf16f292f77c30a5ee28b42e907c
qirong <609413629@qq.com>
2025-03-15 17:14:11 +0800

    githubpage部署测试

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  // base: '/genshin-impact-moon/',
+  base: '/genshin-impact-moon/',
   resolve: {
     alias: [
       { find: '@', replacement: resolve(__dirname, 'src') },
