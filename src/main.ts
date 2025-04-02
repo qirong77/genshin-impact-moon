a37f6ad6e3082054eba0af614c018d71c9c49d77
qirong <609413629@qq.com>
2025-04-02 22:11:23 +0800

    moon

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import "./common/index";
 // import "./groups/solar";
 // import "./groups/background";
 import './components/berlin-noise/index'
+import './components/moon/index'
 import './groups/vedio-icon'
 import { gui } from "./common/gui";
 import { orbitCOntroler } from "./common/oribtControls";
