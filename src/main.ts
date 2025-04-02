d67db2c4d4cf4ca87ff584486445c1bbb0f6b9a0
qirong77 <qirong7777@gmail.com>
2025-04-02 19:38:40 +0800

    调整项目结构

--- src/main.ts
+++ src/main.ts
@@ -1,50 +1,12 @@
 import "./style.css";
 import "./common/index";
-import "./groups/solar";
-import "./groups/background";
-import videoPath from "@assets/audio/bg.mp3";
-import GitHubPath from "@assets/item/github-mark-white.png";
+// import "./groups/solar";
+// import "./groups/background";
+import './groups/vedio-icon'
 import { gui } from "./common/gui";
 import { orbitCOntroler } from "./common/oribtControls";
-
 const isProd = import.meta.env.PROD;
 if (isProd) {
     gui.destroy();
     orbitCOntroler.dispose();
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-    const overlay = document.querySelector('.overlay');
-    if (overlay) {
-        overlay.addEventListener('click', () => {
-            overlay.classList.add('fade-out');
-            addGitHubIcon();
-            addAudio();
-        });
-    }
-});
-
-function addGitHubIcon() {
-    const github = document.createElement("img");
-    github.src = GitHubPath;
-    github.classList.add("github-icon");
-    github.classList.add("github");
-    github.addEventListener("click", () => {
-        window.open("https://github.com/qirong77/genshin-impact-moon", "_blank");
-    });
-    document.body.appendChild(github);
-}
-
-function addAudio() {
- 
-    // 创建音频元素
-    const audio = document.createElement("audio");
-    audio.src = videoPath;
-    audio.loop = true;
-    audio.autoplay = true;
-    // 添加错误处理
-    audio.onerror = (e) => {
-        console.error("音频加载失败:", e);
-    };
-    document.body.appendChild(audio);
 }
