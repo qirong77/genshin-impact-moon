893e5926d3295f6d005eee98fe4767e2b6b85f96
qirong77 <qirong7777@gmail.com>
2025-04-05 17:42:34 +0800

    refactor: 重构代码

--- src/scene/4-Nod-Krai/index.ts
+++ src/scene/4-Nod-Krai/index.ts
@@ -4,17 +4,11 @@ import { createMainImage } from "./card/createMainImage";
 import Fatui from "@assets/Nuodekalai/Fatui.png";
 import { createSceneWheelGui } from "../3-wheel/wheel-gui";
 const gui = createSceneWheelGui("Nod-Krai");
-
-const cardFront = createCard();
-const cardBack = createCard();
-
-function createCard() {
-    const mainImageMesh = createMainImage(gui);
-    // const background = createSatellitesBackground(gui);
-    const group = new THREE.Group();
-    group.add(mainImageMesh);
-    mainImageMesh.material.uniforms.u_time;
-    // group.add(background);
-    group.scale.set(1.2, 1.2, 1.2);
-    scene.add(group);
-}
+const mainImageMesh = createMainImage(gui);
+// const background = createSatellitesBackground(gui);
+const group = new THREE.Group();
+group.add(mainImageMesh);
+mainImageMesh.material.uniforms.u_time;
+// group.add(background);
+group.scale.set(1.2, 1.2, 1.2);
+scene.add(group);
