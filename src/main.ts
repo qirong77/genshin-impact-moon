92ef18a6c3ffce464ddb04e8b74b5f49544a9021
qirong77 <qirong7777@gmail.com>
2025-04-07 01:03:22 +0800

    feat: 功能开发

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import "./style.css";
 import "./common/index";
-// import "./scene/1-mask/index";
-// import "./scene/2-moon/index";
+import "./scene/1-mask/index";
 import "./scene/3-wheel/index";
 import './scene/4-Nod-Krai/index'
 import { gui } from "./common/gui";
